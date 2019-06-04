7aa92c6cda61eab5180227e34ee02a3795288c69
Sergey.Matviychuk <Sergey.Matviychuk@x5.ru>
2019-06-04 12:59:08 +0300

    upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Paper } from './components/index';
+
+export default function App() {
+  return (
+    <div>
+      <h3>Test page</h3>
+      <Paper>
+        <div>This is Paper component</div>
+      </Paper>
+    </div>
+  );
+}
---
+++ src/components/BackButton.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent } from 'react';
+import Router from 'next/router';
+import { IButtonProps } from 'interfaces';
+import { Button, FaIcon } from '.';
+
+interface IBackButtonProps extends IButtonProps {
+  to?: string;
+}
+
+const BackButton: FunctionComponent<IBackButtonProps> = ({ to, children, ...other }) => {
+  function handleClick() {
+    if (to) {
+      Router.push(to);
+    } else {
+      Router.back();
+    }
+  }
+  return (
+    <Button onClick={handleClick} {...other}>
+      <FaIcon icon="arrow-left" className="mr-2" />
+      {children}
+    </Button>
+  );
+};
+
+BackButton.defaultProps = {
+  title: 'Назад',
+};
+
+export default BackButton;
